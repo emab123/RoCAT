4aa4981f969e2d6c26edbc520b81a7f7fc1633c7
Ruben <r.vandenberg0205@gmail.com>
2023-08-01 20:20:10 +0200

    Revert "Initial Commit"
    This reverts commit c429eda111b0680b3ef3e6fc8cbe14294c328075.

--- EngineComponents/Abstract/StructuralComponent.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from EngineComponents.Abstract.Material import Material
-
-
-@dataclass
-class StructuralComponent:
-    structure_material: Material
-    safety_factor: float  # [-]
--- EngineComponents/Other/Inverter.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from EngineComponents.Abstract.ElectricalComponent import ElectricalComponent
-
-
-@dataclass
-class Inverter(ElectricalComponent):
-    pass
